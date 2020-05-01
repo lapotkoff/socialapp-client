2741bfca26999a82764daa7cd29142ac7c016503
lapotkoff <valentyn.lapotkov@agiliway.com>
2020-05-01 16:08:42 +0300

    added initial login flow

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import Link from 'react-router-dom/Link';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
 import withStyles from '@material-ui/core/styles/withStyles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -23,6 +26,7 @@ const styles = {
 
 class Scream extends Component {
   render() {
+      dayjs.extend(relativeTime);
     const { classes, scream: { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount } } = this.props;
     return (
       <Card className={classes.card}>
@@ -36,7 +40,7 @@ class Scream extends Component {
             {userHandle}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            {createdAt.toString()}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">
             {body}
