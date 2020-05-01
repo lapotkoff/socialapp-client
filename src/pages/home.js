2160288654b98f736185410b06c0087a2cefe9bf
lapotkoff <valentyn.lapotkov@agiliway.com>
2020-05-01 13:55:30 +0300

    added initial scream card structure; adjusted home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+
+import Scream from '../components/Scream';
 
 class home extends Component {
+  state = { screams: null };
+  componentDidMount() {
+    axios.get('/screams')
+      .then(res => {
+        console.log(res.data);
+        this.setState({ screams: res.data });
+      })
+      .catch(err => console.error('Error while fetching screams: ', err));
+  }
+
   render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map(scream => <Scream scream={scream} />)
+    ) : <p>Loading...</p>
     return (
-      <h1>Home page</h1>
+      <Grid container spacing={10}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile</p>
+        </Grid>
+      </Grid>
     );
   }
 }
