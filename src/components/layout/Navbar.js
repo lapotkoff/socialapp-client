a71d2e0b809dcd3c113c6f4286a3ee7968633327
lapotkoff <valentyn.lapotkov@agiliway.com>
2020-05-03 17:04:12 +0300

    added 'post a scream' button for logged in user

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import MyButton from '../../util/MyButton';
-// import PostScream from '../scream/PostScream';
+import PostScream from '../scream/PostScream';
 // import Notifications from './Notifications';
 // MUI stuff
 import AppBar from '@material-ui/core/AppBar';
@@ -20,7 +20,7 @@ class Navbar extends Component {
         <Toolbar className="nav-container">
           {authenticated ? (
             <Fragment>
-              {/* <PostScream /> */}
+              <PostScream />
               <Link to="/">
                 <MyButton tip="Home">
                   <HomeIcon />
